9b81deefd898df496906f7b4663fc6a843281f08
chiencoix2004 <tacongchien003@gmail.com>
2024-01-08 15:03:39 +0700

    code update

--- advance/array.php
+++ advance/array.php
@@ -1,21 +1,26 @@
 <?php
 echo "<pre>";
 $array1 = [10,5,15,30];
+echo $array1[2] . PHP_EOL;
+
 $array2 =[
     'name'=> 'Nguyễn Văn B',
-    'age'=> 25
+    'age'=> 25,
 ];
+
+
 
 $array3 = [
-    ['name'=> 'Nguyễn Văn A', 'age', 35];
-    ['name'=> 'Nguyễn Văn B', 'age', 28];
-    ['name'=> 'Nguyễn Văn C', 'age', 96];
+    ['name'=> 'Nguyễn Văn A', 'age'=> 35],
+    ['name'=> 'Nguyễn Văn B', 'age'=>28],
+    ['name'=> 'Nguyễn Văn C', 'age'=> 96],
 
 ];
+echo $array3[1]['age'] . PHP_EOL;
 
 $array4 = [
-    ['name'=> 'Nguyễn Văn A', 'age', 35];
-    ['name'=> 'Nguyễn Văn A', 'age', 28];
+    ['name'=> 'Nguyễn Văn A', 'age', 35],
+    ['name'=> 'Nguyễn Văn A', 'age', 28],
     [
         'name'=>'Nguyễn Văn C',
         'age'=> 96,
@@ -23,10 +28,18 @@ $array4 = [
             'Web2'=>10,
             'PHP2'=>9
         ]
+        ],
         ];
-        ];
+        echo $array4[2]['points']['Web2'] . PHP_EOL;
 
+//Hàm arra-merge - gộp một hoặc nhiều mảng
+$arr1 = [1,4,5,'x' =>'ahihi'];
+$arr2 = ['kkk','keke',['ok'=>'yes']];
+$arr3 =[ 1,4,5,'x' =>'ahihi-array3'];
 
-
+$arrMeg = array_merge($arr1,$arr2,$arr3);
+$arrMeg2 = [...$arr1,...$arr2,...$arr3];
+print_r($arrMeg);
+print_r($arrMeg2);
 
 
--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 echo "Hello World";
 // require_once 'basic/bien.php';
 // require_once 'basic/function.php';
-require_once 'basic/string.php';
+// require_once 'basic/string.php';
+require_once 'advance/array.php';
 ?>
